1076ed9bb23608fe4ff55e38d11adceac4bdc5f5
rayetata <zephaniahraye.belmis-24@cpu.edu.ph>
2026-07-25 15:50:54 +0800

    add school supplies

--- gearhub/src/App.tsx
+++ gearhub/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/home_page";
+import SchoolSupplies from "./pages/school_supplies";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/school-supplies" element={<SchoolSupplies />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- gearhub/src/pages/school_supplies.tsx
+++ gearhub/src/pages/school_supplies.tsx
@@ -1,4 +1,55 @@
-import { usestate } from "react"
-import "..styles/school_supplies.css"
+import "../styles/school_supplies.css";
 
-export default 
+import notebook from "../assets/notebook.jpg";
+import pen from "../assets/pen.jpg";
+import pencil from "../assets/pencil.jpg";
+import eraser from "../assets/eraser.jpg";
+
+export default function SchoolSupplies() {
+  const products = [
+    {
+      id: 1,
+      name: "Notebook",
+      price: "₱75",
+      image: notebook,
+    },
+    {
+      id: 2,
+      name: "Ballpen",
+      price: "₱100",
+      image: pen,
+    },
+    {
+      id: 3,
+      name: "Pencil",
+      price: "₱75",
+      image: pencil,
+    },
+    {
+      id: 4,
+      name: "Eraser",
+      price: "₱50",
+      image: eraser,
+    },
+  ];
+
+  return (
+    <div className="school-page">
+      <h1>School Supplies</h1>
+
+      <div className="products-grid">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <img src={product.image} alt={product.name} />
+
+            <h3>{product.name}</h3>
+
+            <p>{product.price}</p>
+
+            <button>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
